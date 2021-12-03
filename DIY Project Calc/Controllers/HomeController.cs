a1179f33288eec79840a2cc844fc4313beb113fb
Steve Nyholm <stevenyholm@gmail.com>
2021-12-03 12:16:51 -0800

    Add project files.

---
+++ DIY Project Calc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DIY_Project_Calc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DIY_Project_Calc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DIY Project Calc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DIY_Project_Calc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
