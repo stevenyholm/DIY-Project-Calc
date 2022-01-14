25af80a54196e53a9aa0c441d13e804ece3cf10f
Steve Nyholm <stevenyholm@gmail.com>
2022-01-13 17:03:13 -0800

    add DepthNeeded to Material

---
+++ DiyProjectCalc/Migrations/20220114010141_added_unit_tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiyProjectCalc.Migrations
+{
+    public partial class added_unit_tests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DepthNeeded",
+                table: "Material",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepthNeeded",
+                table: "Material");
+        }
+    }
+}
