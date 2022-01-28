e728b617342dc395e4ad9052ee5c9010e41b2c04
Steve Nyholm <stevenyholm@gmail.com>
2022-01-28 00:08:30 -0800

    added integration tests project

---
+++ DiyProjectCalc.IntegrationTests/Controllers/Abstractions/ControllerTestsBase.cs
@@ -0,0 +1,25 @@
+﻿using DiyProjectCalc.Data;
+using DiyProjectCalc.IntegrationTests.TestFixtures;
+using DiyProjectCalc.Tests.TestData;
+using System.Linq;
+using Xunit;
+
+namespace DiyProjectCalc.IntegrationTests.Controllers.Abstractions;
+
+public abstract class ControllerTestsBase : IClassFixture<DefaultTestDatabaseFixture>
+{
+    public DefaultTestDatabaseFixture Fixture { get; }
+
+    protected ControllerTestsBase(DefaultTestDatabaseFixture fixture) => Fixture = fixture;
+
+    protected ApplicationDbContext NewDbContext() => Fixture.CreateContext();
+
+    protected int ValidMaterialId(ApplicationDbContext dbContext) =>
+        dbContext.Materials.FirstOrDefault(m => m.Name == MaterialsTestData.NameValid)?.MaterialId ?? 0;
+
+    protected int ValidProjectId(ApplicationDbContext dbContext) =>
+        dbContext.Projects.FirstOrDefault(m => m.Name == ProjectsTestData.NameValid)?.ProjectId ?? 0;
+
+    protected int ValidBasicShapeId(ApplicationDbContext dbContext) =>
+        dbContext.BasicShapes.FirstOrDefault(m => m.Name == BasicShapesTestData.NameValid)?.BasicShapeId ?? 0;
+}
---
+++ DiyProjectCalc.IntegrationTests/TestFixtures/DefaultTestDatabaseFixture.cs
@@ -0,0 +1,47 @@
+﻿using DiyProjectCalc.Data;
+using DiyProjectCalc.Tests.TestData;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiyProjectCalc.IntegrationTests.TestFixtures;
+
+public class DefaultTestDatabaseFixture
+{
+    private const string ConnectionString = @"Server=steve-dell;Database=DiyProjectCalcTests;Trusted_Connection=true";
+
+    private static readonly object _lock = new();
+    private static bool _databaseInitialized;
+
+    public DefaultTestDatabaseFixture()
+    {
+        lock (_lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using (var context = CreateContext())
+                {
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+
+                    seedInitialTestData(context);
+                }
+
+                _databaseInitialized = true;
+            }
+        }
+    }
+
+    public ApplicationDbContext CreateContext()
+        => new ApplicationDbContext(
+            new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+
+    private void seedInitialTestData(ApplicationDbContext dbContext)
+    {
+        var projectsTestData = new ProjectsTestData();
+        var projects = ProjectsTestData.ProjectsFor(projectsTestData.TestDataForAllProjects);
+        dbContext.Projects.AddRange(projects);
+
+        dbContext.SaveChanges();
+    }
+}
